75efef78a7344f872854f0fd81de64c3c9b82bbd
Deepakar13 <deepankar.sora@gmail.com>
2025-07-09 13:24:58 +0530

    final

--- client/src/common/AppNavBar.js
+++ client/src/common/AppNavBar.js
@@ -10,15 +10,18 @@ import styled from 'styled-components';
 const Nav = styled.nav`
   background: ${(props) => props.theme.gray};
   color: ${(props) => props.theme.light};
-  height: 60px;
   width: 100%;
+  height: 60px;
   position: fixed;
+  top: 0;
   z-index: 12;
+  display: flex;
+  align-items: center;
 `;
 
 const NavInnerContainer = styled.div`
   max-width: 1100px;
-  height: 100%;
+  width: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -26,24 +29,23 @@ const NavInnerContainer = styled.div`
   padding: 0 1rem;
 `;
 
-const LogoContainer = styled.div`
+const LeftSection = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
-  border-radius: 50%;
-  font-size: 1.25rem;
-  height: 37px;
-  width: 42px;
-  background: ${(props) => props.theme.dark};
-  color: ${(props) => props.theme.gold};
+  gap: 1rem;
 `;
 
-const NavList = styled.ul`
+const CenterSection = styled.div`
   display: flex;
-  justify-content: space-between;
-  align-items: flex-end;
+  justify-content: center;
+  flex-grow: 1;
+`;
+
+const RightSection = styled.ul`
+  display: flex;
   list-style: none;
-  margin-bottom: 0;
+  margin: 0;
+  padding: 0;
 
   a {
     text-decoration-color: ${(props) => props.theme.gold};
@@ -58,10 +60,21 @@ const NavItem = styled.li`
   position: relative;
 `;
 
+const LogoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  font-size: 1.25rem;
+  height: 37px;
+  width: 42px;
+  background: ${(props) => props.theme.dark};
+  color: ${(props) => props.theme.gold};
+`;
+
 const CustomBadge = styled(Badge)`
   color: ${(props) => props.theme.light};
   background: hsl(46, 80%, 40%);
-  vertical-align: baseline;
   padding-right: 0.6em;
   padding-left: 0.55em;
   margin-left: 0.3rem;
@@ -86,27 +99,54 @@ const GithubIcon = styled(FaGithub)`
   margin-left: 2rem;
 `;
 
-const AppNavbar = () => {
+const SearchInput = styled.input`
+  padding: 0.4rem 0.75rem;
+  border-radius: 5px;
+  border: none;
+  width: 200px;
+  font-size: 0.9rem;
+  outline: none;
+`;
+
+const AppNavbar = ({ onSearchChange }) => {
   const { watchlist } = useSelector(watchlistState);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (e) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+    if (onSearchChange) onSearchChange(value);
+  };
 
   return (
     <Nav>
       <NavInnerContainer>
-        <Link to='/'>
-          <LogoContainer>
-            <GiPopcorn />
-          </LogoContainer>
-        </Link>
-        <NavList>
+        <leftSection>
           <Link to='/'>
-            <NavItem>Home</NavItem>
+            <LogoContainer>
+              <GiPopcorn />
+            </LogoContainer>
           </Link>
+        </leftSection>
+
+
+        <centerSection>
+          <SearchInput
+            type='text'
+            placeholder='Search movies...'
+            value={searchTerm}
+            onChange={handleSearch}
+          />
+        </centerSection>
+
+        
+
+        <RightSection>
+          <Link to='/'><NavItem>Home</NavItem></Link>
           <Link to='/watchlist'>
             <NavItem>
               Watchlist
-              <CustomBadge pill>
-                <span>{watchlist.length}</span>
-              </CustomBadge>
+              <CustomBadge pill><span>{watchlist.length}</span></CustomBadge>
             </NavItem>
           </Link>
           <NavItem>
@@ -118,7 +158,7 @@ const AppNavbar = () => {
               <GithubIcon />
             </a>
           </NavItem>
-        </NavList>
+        </RightSection>
       </NavInnerContainer>
     </Nav>
   );
